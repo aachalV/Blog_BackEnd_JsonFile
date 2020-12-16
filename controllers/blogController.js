30e89e58f12431eb569ae8e1f537bf59b5e21381
aachalv <aachal.vartak@raweng.com>
2020-12-17 02:06:10 +0530

    Get blog by id implemented

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -8,8 +8,40 @@ const sendErrorMessage = require("../helpers/sendError");
 const fileName = path.join(__dirname, "../data", "blogs.json");
 const blogs = JSON.parse(fs.readFileSync(fileName, "utf-8"));
 
+//Get All Blogs
 const getAllBlogs = (req, res, next) => {
   sendResponse(200, "Sucessful", blogs, req, res);
 };
+//Check Id
+const isIdValid = (req, res, next) => {
+  console.log("Enterd IsVAlid");
+  let id = req.params.id;
+  let idExists = blogs.some((blog) => {
+    return blog.id == id;
+  });
+  if (!idExists) {
+    return sendErrorMessage(
+      new AppError(
+        400,
+        "Unsuccessful",
+        "Request Body Invalid Input...Id doesn't exists"
+      ),
+      req,
+      res
+    );
+  } else {
+    next();
+  }
+};
+//Get Blog by Id
+const getBlogById = (req, res, next) => {
+  let id = req.params.id;
+  let blog = blogs.find((blog) => {
+    return blog.id == id;
+  });
+  sendResponse(200, "Successfull", blog, req, res);
+};
 
 module.exports.getAllBlogs = getAllBlogs;
+module.exports.isIdValid = isIdValid;
+module.exports.getBlogById = getBlogById;
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { getAllBlogs } = require("../controllers/blogController");
+const {
+  getAllBlogs,
+  isIdValid,
+  getBlogById,
+} = require("../controllers/blogController");
 
 const router = express.Router();
 
 //End points
 router.route("/blogs").get(getAllBlogs);
+router.route("/blogs/:id").get(isIdValid, getBlogById);
 
 module.exports = router;
